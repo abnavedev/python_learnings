64a5135f0a35ff2c0f87838d5f8c0cb28affdddd
abnavedev <abnavedev@gmail.com>
2021-09-02 19:34:30 +0530

    a

--- XML_parser.py
+++ XML_parser.py
@@ -3,12 +3,20 @@
 # XML is used to for data presentataion , used to send data between client and servers
 
 # Parsing means to read info and split it into pieces by identifying parts 
-# modile ElementTree
+# Parsing can be done using 2 modules 
+# tElementTree i.e xml.etree.ElementTree module and Minidom (Minimal DOM Implementation)
 
+
+# xml.etree.ElementTree Module:
+#  format XML data in a tree structure
 import xml.etree.ElementTree as ET
+
+
+# Using parse() function:
 
 tree = ET.parse('sample.xml')
 
-root = tree.getroot()
+root = tree.getroot() #getroot( returns the root element
 
 
+# edureka
