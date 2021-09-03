ef468d7a783a7388b6485f5d1781416abe7691c9
abnavedev <abnavedev@gmail.com>
2021-09-03 22:45:19 +0530

    A

--- DecentDirectory.py
+++ DecentDirectory.py
@@ -1,4 +1,3 @@
-# Python File I/O
 # open() function  - returns a file object
 # can specify the mode
 # r 
@@ -25,11 +24,25 @@ print(os.getcwd())
 
 # os.listdir() to get the list of all files and directories in the specified directry
 
+
 # traverse root directory, and list directories as dirs and files as files
+# os.walk returns a generator, that creates a tuple of values (current_path, directories in current_path, files in current_path).
+
+
+
+import os
+
+for root, dirs, files in os.walk("/home/abhis/Documents/Test_Directory"):
+   for name in files:
+      print(os.path.join(root, name))
+   for name in dirs:
+      print(os.path.join(root, name))
+
+
+
 for root, dirs, files in os.walk("/home/abhis/Documents/Test_Directory"):
     path = root.split(os.sep)
-    print((len(path) - 1) * '---', os.path.basename(root))
+    print((len(path) - 1) * '+++', os.path.basename(root))
     for file in files:
-        print(len(path) * '---', file)
-
+        print(len(path) * '***', file)
 
